84e766c476054ac646255ad12838768c3bff8c5c
Rajatluharuka <rajatluharuka123>
2024-07-21 16:24:15 +0530

    Task 6 updated

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,29 @@
+import { useContext, useRef } from 'react';
+import CartContext from '../../../store/cart-context';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
+  const cartCtx = useContext(CartContext);
+  const amountInputRef = useRef();
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    const item = {
+      id: props.id,
+      name: props.name,
+      amount: enteredAmountNumber,
+      price: props.price,
+    };
+
+    cartCtx.addItem(item);
+  };
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label='Amount'
         input={{
           id: 'amount_' + props.id,
